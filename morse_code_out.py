b38715f76d685a95624a9084c6b38c9ffe611d2c
BenKinchin <49315211+BenKinchin@users.noreply.github.com>
2019-07-18 11:35:32 +0100

    Update morse_code_out.py

--- morse_code_out.py
+++ morse_code_out.py
@@ -10,7 +10,7 @@ def setup():
 	
 letters_list = '?'.join("abcdefghijklmnopqrstuvwxyz").split("?")
 morse_list = ('01', '1000', '1010', '100', '0', '0010', '110', '0000', '00', '0111', '101', '0100', '11', '10', '111', '0110', '1101', '010', '000', '1', '001', '0001', '011', '1001', '1011', '1100')
-dot = 0.3
+dot = 0.4
 
 def message_time():
 	morse_message = ''
